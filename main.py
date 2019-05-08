f6da2e059863bd0b63bdc39aa8a2f74ef049dead
Alberto <albm@itu.dk>
2019-05-08 18:29:47 +0200

    basic keras model example

--- main.py
+++ main.py
@@ -2,7 +2,15 @@ from model.keras_model import *
 
 
 def main():
-    print(score)
+    x_train = np.random.random((1000, 20))
+    y_train = keras.utils.to_categorical(np.random.randint(10, size=(1000, 1)), num_classes=10)
+    x_test = np.random.random((100, 20))
+    y_test = keras.utils.to_categorical(np.random.randint(10, size=(100, 1)), num_classes=10)
+
+    model = create_model()
+    result = train_model(model, x_train, y_train, x_test, y_test)
+
+    print('Loss & Accuracy: ', result)
 
 
 if __name__ == "__main__":
