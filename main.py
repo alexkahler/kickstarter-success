b11412e1c55b209c732442d0b0de02d63a3b41d5
Alberto <albm@itu.dk>
2019-05-08 18:49:08 +0200

    get_data() method

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 from model.keras_model import *
-import numpy as np
-import keras
 
 
 def main():
-    x_train = np.random.random((1000, 20))
-    y_train = keras.utils.to_categorical(np.random.randint(10, size=(1000, 1)), num_classes=10)
-    x_test = np.random.random((100, 20))
-    y_test = keras.utils.to_categorical(np.random.randint(10, size=(100, 1)), num_classes=10)
 
+    x_train, y_train, x_test, y_test = get_data()
     model = create_model()
     result = train_model(model, x_train, y_train, x_test, y_test)
 
--- model/keras_model.py
+++ model/keras_model.py
@@ -1,6 +1,17 @@
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
 from keras.optimizers import SGD
+import numpy as np
+import keras
+
+
+def get_data():
+    x_train = np.random.random((1000, 20))
+    y_train = keras.utils.to_categorical(np.random.randint(10, size=(1000, 1)), num_classes=10)
+    x_test = np.random.random((100, 20))
+    y_test = keras.utils.to_categorical(np.random.randint(10, size=(100, 1)), num_classes=10)
+
+    return x_train, y_train, x_test, y_test
 
 
 def create_model():
@@ -24,13 +35,9 @@ def train_model(model, x_train, y_train, x_test, y_test):
                   metrics=['accuracy'])
 
     model.fit(x_train, y_train,
-              epochs=20,
+              epochs=50,
               batch_size=128)
 
     score = model.evaluate(x_test, y_test, batch_size=128)
 
     return score
-
-
-
-
