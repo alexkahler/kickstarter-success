bcb0823a1c2fe6b27088d937d963a40637169ab9
Alberto <albm@itu.dk>
2019-05-14 11:58:58 +0200

    adapted simple model to binary classification (succes/fail)

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ from model.keras_model import *
 
 
 def main():
-
     x_train, y_train, x_test, y_test = get_data()
     model = create_model()
     result = train_model(model, x_train, y_train, x_test, y_test)
+    cm = predict_model(model, x_test, y_test)
 
     print('Loss & Accuracy: ', result)
+    print(cm)
 
 
 if __name__ == "__main__":
--- model/keras_model.py
+++ model/keras_model.py
@@ -1,15 +1,16 @@
+import keras
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
-from keras.optimizers import SGD
+from keras.optimizers import SGD, Adam
 import numpy as np
-import keras
+from sklearn.metrics import confusion_matrix
 
 
 def get_data():
-    x_train = np.random.random((1000, 20))
-    y_train = keras.utils.to_categorical(np.random.randint(10, size=(1000, 1)), num_classes=10)
-    x_test = np.random.random((100, 20))
-    y_test = keras.utils.to_categorical(np.random.randint(10, size=(100, 1)), num_classes=10)
+    x_train = np.random.random((1000, 50))
+    y_train = np.random.choice([0, 1], size=(1000,))
+    x_test = np.random.random((200, 50))
+    y_test = np.random.choice([0, 1], size=(200,))
 
     return x_train, y_train, x_test, y_test
 
@@ -18,26 +19,27 @@ def create_model():
     model = Sequential()
     # Dense(64) is a fully-connected layer with 64 hidden units.
     # in the first layer, you must specify the expected input data shape:
-    # here, 20-dimensional vectors.
-    model.add(Dense(64, activation='relu', input_dim=20))
+    # here, 50-dimensional vectors.
+    model.add(Dense(64, activation='relu', input_dim=50))
     model.add(Dropout(0.5))
     model.add(Dense(64, activation='relu'))
     model.add(Dropout(0.5))
-    model.add(Dense(10, activation='softmax'))
+    model.add(Dense(1, activation='sigmoid'))
 
     return model
 
 
 def train_model(model, x_train, y_train, x_test, y_test):
-    sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-    model.compile(loss='categorical_crossentropy',
-                  optimizer=sgd,
-                  metrics=['accuracy'])
-
-    model.fit(x_train, y_train,
-              epochs=50,
-              batch_size=128)
-
-    score = model.evaluate(x_test, y_test, batch_size=128)
+    # sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.fit(x_train, y_train, epochs=100, batch_size=10)
+    score = model.evaluate(x_test, y_test, batch_size=10)
 
     return score
+
+
+def predict_model(model, x_test, y_test):
+    y_pred = model.predict(x_test)
+    y_pred = (y_pred > 0.5)
+    cm = confusion_matrix(y_test, y_pred)
+    return cm
