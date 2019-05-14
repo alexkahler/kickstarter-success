0a371c174d8b423e939e92a58271231e48c369e7
Alberto <albm@itu.dk>
2019-05-14 14:24:54 +0200

    cross-validation

--- main.py
+++ main.py
@@ -2,13 +2,22 @@ from model.keras_model import *
 
 
 def main():
+    # Get data
     x_train, y_train, x_test, y_test = get_data()
-    model = create_model()
-    result = train_model(model, x_train, y_train, x_test, y_test)
-    cm = predict_model(model, x_test, y_test)
+    X = np.concatenate((x_train, x_test))
+    Y = np.concatenate((y_train, y_test))
 
+    # Create and train model
+    model = create_model()
+    trained_model = train_model(model, x_train, y_train, 100, 20)
+
+    # Evaluate model
+    result = evaluate_model(trained_model, x_test, y_test, 20)
+    cm = predict_model(trained_model, x_test, y_test)
     print('Loss & Accuracy: ', result)
-    print(cm)
+    print('Confusion Matrix:', cm)
+
+    cross_validation(X, Y, 2)
 
 
 if __name__ == "__main__":
--- model/keras_model.py
+++ model/keras_model.py
@@ -1,10 +1,15 @@
-import keras
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
-from keras.optimizers import SGD, Adam
-import numpy as np
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import cross_val_score
+from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import confusion_matrix
+from sklearn.pipeline import Pipeline
+import numpy as np
 
+
+input_dim = 0
 
 def get_data():
     x_train = np.random.random((1000, 50))
@@ -17,23 +22,24 @@ def get_data():
 
 def create_model():
     model = Sequential()
-    # Dense(64) is a fully-connected layer with 64 hidden units.
-    # in the first layer, you must specify the expected input data shape:
-    # here, 50-dimensional vectors.
-    model.add(Dense(64, activation='relu', input_dim=50))
+    model.add(Dense(64, activation='relu', kernel_initializer='normal', input_dim=50))
     model.add(Dropout(0.5))
-    model.add(Dense(64, activation='relu'))
+    model.add(Dense(64, activation='relu', kernel_initializer='normal'))
     model.add(Dropout(0.5))
-    model.add(Dense(1, activation='sigmoid'))
+    model.add(Dense(1, kernel_initializer='normal', activation='sigmoid'))
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
     return model
 
 
-def train_model(model, x_train, y_train, x_test, y_test):
-    # sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-    model.fit(x_train, y_train, epochs=100, batch_size=10)
-    score = model.evaluate(x_test, y_test, batch_size=10)
+def train_model(model, x_train, y_train, epochs=50, batch_size=25):
+    model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size)
+
+    return model
+
+
+def evaluate_model(model, x_test, y_test, batch_size=25):
+    score = model.evaluate(x_test, y_test, batch_size=batch_size)
 
     return score
 
@@ -41,5 +47,16 @@ def train_model(model, x_train, y_train, x_test, y_test):
 def predict_model(model, x_test, y_test):
     y_pred = model.predict(x_test)
     y_pred = (y_pred > 0.5)
-    cm = confusion_matrix(y_test, y_pred)
-    return cm
+
+    return confusion_matrix(y_test, y_pred)
+
+
+def cross_validation(X, Y, n_splits=10):
+    print('Running Cross-Valiation')
+    estimators = []
+    estimators.append(('standardize', StandardScaler()))
+    estimators.append(('mlp', KerasClassifier(build_fn=create_model, epochs=25, batch_size=5, verbose=0)))
+    pipeline = Pipeline(estimators)
+    kfold = StratifiedKFold(n_splits=n_splits, shuffle=True)
+    results = cross_val_score(pipeline, X, Y, cv=kfold)
+    print('Results: %.2f%% (%.2f%%)' % (results.mean()*100, results.std()*100))
