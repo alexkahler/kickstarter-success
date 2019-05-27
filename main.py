f11c3097ba8214efe3cf7b38d9b4a737ad4fd914
Spejderman <r.odgaard@gmail.com>
2019-05-27 11:25:20 +0200

    final project source code

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
 from model.keras_model import *
-import numpy as np
-import sklearn
-
 
 def main():
-    # Get data
-    X, Y = get_data()
-    # X, Y = get_slim_data()
+    # Get data:
 
-    # Create and train model
-    # model = create_model()
-    # trained_model = train_model(model, x_train, y_train, 150, 256)
+    #shows distribution of failed/success for categories food, games and technology
+    get_distribution()
 
-    # Evaluate model
-    # result = evaluate_model(trained_model, x_test, y_test, 128)
-    # cm = predict_model(trained_model, x_test, y_test)
-    # print('Loss & Accuracy: ', result)
-    # print('Confusion Matrix:', cm)
+    #Get all data for simple/alt model
+    #X, Y = get_data()
 
+    #Get "slim" data for simple/alt model
+    X,Y = get_slim_data()
 
-    # cross_validation(X, Y, k, model, epochs)
+    #Get "slim" data for category model, that is balanced for binary classification
+    # 1st argument is category you wish to test, 2nd argument is column to exclude from test
+    #X,Y = get_balanced_slim_cat('category_food', 'usd_goal')
 
-    cross_validation(X, Y, 5, 'simple', 100)
+    #Runs dummy prediction for baseline accuracy
+    dummy_cross_validation(X, Y, 5)
+
+    # cross_validation(X, Y, k, model, epochs, batch_size)
+    cross_validation(X, Y, 5, 'simple', 100, 64)
 
 
 if __name__ == "__main__":
